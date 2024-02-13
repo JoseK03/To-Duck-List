a72847b15bfe9079437f33ce06144951c6f8d847
JoseK03 <dajoseva@gmail.com>
2024-02-13 14:07:29 -0500

    feat: :construction: Creacion de crud para tareas

--- Backend/src/controllers/task.controller.js
+++ Backend/src/controllers/task.controller.js
@@ -9,3 +9,19 @@ const getTask = async (req,res) => {
         res.status(404).json({message : 'Error al encontrar las tareas'});
     }
 }
+
+const postTask = async (req,res) => {
+    const {title, description} = req.body;
+    try {
+        const newTask = new Task({
+            title,
+            description,
+            date
+        });
+        const task = await newTask.save();
+        res.json(task);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message : 'Error al registrar la tarea.'})   
+    }
+}
