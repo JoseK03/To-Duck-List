bcfa00589ac73aac9e9c0c905d499f965408804e
JoseK03 <dajoseva@gmail.com>
2024-02-14 15:48:09 -0500

    feat: :wrench: instanciamos cors
    Se instancia cors para permitir enlazar el frontend con el backend en ambdos dominios

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,10 +4,14 @@ import morgan from 'morgan'
 import userRouter from './routes/user.routes.js'
 import cookieParser from 'cookie-parser';
 import taskRouter from './routes/task.routes.js';
+import cors from 'cors';
 
 dotenv.config()
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
 app.use(morgan('combined'));//para seguimiento detallado de solicitudes HTTP
 app.use(express.json());
 app.use(cookieParser());
