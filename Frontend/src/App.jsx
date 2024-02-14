1f06cc52dcd8cc519cdf94fee7036586021320fd
JoseK03 <dajoseva@gmail.com>
2024-02-14 16:53:47 -0500

    feat: :construction: Proceso de validacion de datos por frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import RegisterPage from "./pages/RegisterPage"
 import LoginPage from "./pages/LoginPage"
+import { AuthProvider } from "./context/AuthContext"
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home page</h1>} />
-        <Route path="/register" element={<RegisterPage/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/task" element={<h1>Task page</h1>} />
-        <Route path="/add-task" element={<h1>new task</h1>} />
-        <Route path="/task/:id" element={<h1>update task</h1>} />
-        <Route path="/profile" element={<h1>user profile</h1>} />
-
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>} />
+          <Route path="/register" element={<RegisterPage/>} />
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/task" element={<h1>Task page</h1>} />
+          <Route path="/add-task" element={<h1>new task</h1>} />
+          <Route path="/task/:id" element={<h1>update task</h1>} />
+          <Route path="/profile" element={<h1>user profile</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
--- Frontend/src/pages/RegisterPage.jsx
+++ Frontend/src/pages/RegisterPage.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import {useForm} from 'react-hook-form'
-import { registerRequest } from '../api/auth'
+import { useAuth } from '../context/AuthContext'
+
 const RegisterPage = () => {
     
     const {register, handleSubmit} = useForm()
+    const {singup, user} = useAuth();
 
 
   return (
     <>
         <form onSubmit={handleSubmit(async(values) => {
-            console.log(values);
-            const res = await registerRequest(values);
-            console.log(res);
+                singup(values);
+                console.log(user);
             })}>
             <input type="text" {...register('username',{required: true})}placeholder='username' />
             <input type="email" {...register('email',{required: true})} placeholder='email' />
